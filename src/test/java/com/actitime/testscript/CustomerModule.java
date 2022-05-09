7f9af7973739afeb2ecae01f9cf17a29ee08392c
Shubham <Shubham@LAPTOP-7CB9RIFL>
2022-05-09 18:24:11 +0530

    firstbuild

---
+++ src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void createCustomer()
+{
+	Reporter.log("Create Customer",true);
+	
+}
+}
